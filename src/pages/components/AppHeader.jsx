036be0c4d80c986e1318d6c1795b11ed85965876
Andrei Trunkin <inboxavt@gmail.com>
2023-07-03 23:04:29 +0200

    Created folder for components/pages and header

---
+++ src/pages/components/AppHeader.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import i18next from '../../../i18next.js';
+
+const styles = StyleSheet.create({
+  header: {
+    height: 90,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: '#009999',
+  },
+  text: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+  },
+});
+
+const AppHeader = () => {
+  const codeJSX = (
+    <View style={styles.header}>
+      <Text style={styles.text}>{i18next.t('pageOne.header')}</Text>
+    </View>
+  );
+
+  return codeJSX;
+};
+
+export default AppHeader;
---
+++ src/pages/page_1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+// import { useTranslation } from 'react-i18next';
+
+import AppHeader from './components/AppHeader';
+
+const PageOne = () => {
+  const codeJSX = (
+    <View>
+      <AppHeader />
+    </View>
+  );
+
+  return codeJSX;
+};
+
+export default PageOne;
