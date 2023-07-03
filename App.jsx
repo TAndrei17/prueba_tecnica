e1943b2b37141a1fedef8109bb32a26ab3a2f6dd
Andrei Trunkin <inboxavt@gmail.com>
2023-07-03 23:02:52 +0200

    Updated App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,16 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/style-prop-object */
+/* eslint-disable react/function-component-definition */
+
 import { Client } from 'rollbar-react-native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 
-import store from './src/slices'; 
+import UpdateState from './src/api/update_state.js';
+import store from './src/slices';
+
+import PageOne from './src/pages/page_1.jsx';
 
 const rollbar = new Client('e19c73811ae7448cab4c72b06f534dd5');
 rollbar.log('Hello, World!');
@@ -11,19 +18,15 @@ rollbar.log('Hello, World!');
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Hello, World! I am creating!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <UpdateState>
+        <View style={styles.container}>
+          <PageOne />
+        </View>
+      </UpdateState>
     </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
