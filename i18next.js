bf97b526f9e96f6d39408c8c41a32c8e3e887b37
Andrei Trunkin <inboxavt@gmail.com>
2023-07-04 21:06:13 +0200

    Got lng through context

--- i18next.js
+++ i18next.js
@@ -1,12 +1,16 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
 import resources from './src/locales/index.js';
+import { statusState } from './src/context/index.js';
+
+const { lng } = statusState;
 
 i18next
   .use(initReactI18next)
   .init({
     compatibilityJSON: 'v3',
-    lng: 'sp',
+    lng,
     debug: true,
     resources,
     interpolation: {
